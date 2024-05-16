6c334bc1d2cc99c39c8f67c29e56568e8b9349fa
wasifibnharun <131335242+wasifibnharun@users.noreply.github.com>
2024-05-16 13:22:22 +0600

    Just a little modification

--- Homework 6/functions.py
+++ Homework 6/functions.py
@@ -1,17 +1,17 @@
 def discount(t_price):
-    disc1=t_price-(t_price*0.10)
-    print(f"You are given 10% discount and the payable price is {disc1}")
+    discount_price=t_price-(t_price*0.10)
+    print(f"You are given 10% discount and the payable price is {discount_price}")
     
 def payment(tot_price):
     payment_method=input("By which medium are you wanting to pay?\n")
     if  payment_method=="Bkash":
-        price=tot_price*.05
-        print(f"Congratulations,you got a cashback of {price} tk")
+        cashback=tot_price*.05
+        print(f"Congratulations,you got a cashback of {cashback} tk")
     elif payment_method=="Nagad":
-        price=tot_price*.03
-        print(f"Congratulations,you got a cashback of {price} tk")
+        cashback=tot_price*.03
+        print(f"Congratulations,you got a cashback of {cashback} tk")
     elif payment_method=="Rocket":
-        price=tot_price*.04
-        print(f"Congratulations,you got a cashback of {price} tk")
+        cashback=tot_price*.04
+        print(f"Congratulations,you got a cashback of {cashback} tk")
     else:
         print(f"Sorry,we don't have any cashback for {payment_method} payment")
