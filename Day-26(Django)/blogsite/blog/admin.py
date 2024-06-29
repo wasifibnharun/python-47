48dd4d2b252c8976f7b9a6c57ce733e37545f84f
wasifibnharun <wasifibnharun@gmail.com>
2024-06-29 09:36:01 +0600

    added custom css and automations

--- Day-26(Django)/blogsite/blog/admin.py
+++ Day-26(Django)/blogsite/blog/admin.py
@@ -6,5 +6,8 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'status', 'created_on')
     list_filter = ('status',)
     search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    preserve_filters = ('status',)
 
 admin.site.register(Post,PostAdmin)
--- Day-26(Django)/blogsite/blog/models.py
+++ Day-26(Django)/blogsite/blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-STATUS = ((0, 'Draft'), (1, 'Publish'))
+STATUS = ((0, 'Draft'), (1, 'Published'))
 
 class Post(models.Model):
     title = models.CharField(max_length=200,unique=True)
@@ -11,6 +11,7 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
+    image_url = models.CharField(max_length=1083)
     status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
